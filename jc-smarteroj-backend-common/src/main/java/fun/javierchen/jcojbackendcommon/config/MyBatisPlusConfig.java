3f2b788f96ef292e7c061acca11c57c037be95bc
JiaWei-Chen-2295 <1601020332@qq.com>
2026-01-23 08:38:30 +0800

    fix(BindingException): 修复 MyBatis 配置问题

--- jc-smarteroj-backend-common/src/main/java/fun/javierchen/jcojbackendcommon/config/MyBatisPlusConfig.java
+++ jc-smarteroj-backend-common/src/main/java/fun/javierchen/jcojbackendcommon/config/MyBatisPlusConfig.java
@@ -3,7 +3,6 @@ package fun.javierchen.jcojbackendcommon.config;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,7 +12,6 @@ import org.springframework.context.annotation.Configuration;
  * @author https://github.com/liyupi
  */
 @Configuration
-@MapperScan("fun.javierchen")
 public class MyBatisPlusConfig {
 
     /**
